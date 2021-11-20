b67a0dd399d2328f526eafc0ec1db69de892b511
Mikita Byshniou <nick.byshnev@gmail.com>
2021-11-21 02:46:51 +0300

    add JavaSyntax Leve 2 Tasks & Solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 12, numericValue = 4, size = 3, number = 55;
+    }
+}
