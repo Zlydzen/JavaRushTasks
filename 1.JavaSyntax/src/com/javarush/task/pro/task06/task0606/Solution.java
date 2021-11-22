c37211cf14b7403d5fc288f53aba0f1e050d9fcc
Mikita Byshniou <nick.byshnev@gmail.com>
2021-11-22 04:30:22 +0300

    add JavaSyntax Level 7 Tasks & Solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int a) {
+    }
+
+    public static void universalMethod(int a, int b) {
+    }
+
+    public static void universalMethod(boolean isHere) {
+    }
+
+    public static void universalMethod(int[] array) {
+    }
+
+    public static void universalMethod(int[][] array) {
+    }
+
+    public static void universalMethod(String firstName, String lastName) {
+    }
+
+    public static void universalMethod(String[][][] game3D) {
+    }
+
+    public static void universalMethod(int age, String name) {
+    }
+
+    public static void universalMethod(double shoeSize) {
+    }
+
+    public static void universalMethod(long cash) {
+    }
+
+    //напишите тут ваш код
+}
