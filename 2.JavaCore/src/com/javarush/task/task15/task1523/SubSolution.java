dc1490fe485969c2cf9eb52ac49a56c4bf11c39f
Mikita Byshniou <nick.byshnev@gmail.com>
2021-11-26 20:05:51 +0300

    add solution in Task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution(int q) {}
+    protected SubSolution(String line) {}
+    SubSolution() {}
+}
