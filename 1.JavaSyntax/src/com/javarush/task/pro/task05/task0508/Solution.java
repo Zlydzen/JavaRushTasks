b9a4225e5a833938b99b01fb0b0fcbe5999ce60b
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-26 23:48:54 +0300

    add solution in task1704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task05.task0508;
+
+import java.util.Scanner;
+
+/* 
+Удаляем одинаковые строки
+*/
+
+public class Solution {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < strings.length; i++) {
+            String next = scanner.next();
+        }
+
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
