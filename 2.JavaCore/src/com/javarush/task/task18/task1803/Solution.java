ddc8e75cf6f38b6590c2ddcdeda351edbec1cdf7
Zlydzen <nick.byshnev@gmail.com>
2022-01-18 11:01:50 +0300

    read file from console and add all bytes in list

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /* 
 Самые частые байты
@@ -11,5 +13,16 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        List<Integer> listBytes = new ArrayList<>();
+
+        while (fileInputStream.available() > 0){
+            int read = fileInputStream.read();
+            listBytes.add(read);
+        }
+
     }
 }
