3a93260811d0a550596b994fe65994411000b094
Zlydzen <nick.byshnev@gmail.com>
2022-01-18 13:00:39 +0300

    add solution in task1803

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -3,8 +3,7 @@ package com.javarush.task.task18.task1803;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /* 
@@ -17,12 +16,21 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = bufferedReader.readLine();
         FileInputStream fileInputStream = new FileInputStream(fileName);
-        List<Integer> listBytes = new ArrayList<>();
+        byte [] listAllBytes = new byte [256];
 
-        while (fileInputStream.available() > 0){
-            int read = fileInputStream.read();
-            listBytes.add(read);
+        while (fileInputStream.available() > 0) {
+            listAllBytes[fileInputStream.read()]++;
         }
 
+        byte maxRepeat = 0;
+        for (int i = 0; i <listAllBytes.length  ; i++) {
+            if (listAllBytes[i]>maxRepeat) maxRepeat = listAllBytes[i];
+        }
+        for (int i = 0; i <listAllBytes.length ; i++) {
+            if (listAllBytes[i]==maxRepeat) System.out.print(i + " ");
+        }
+
+        bufferedReader.close();
+        fileInputStream.close();
     }
 }
